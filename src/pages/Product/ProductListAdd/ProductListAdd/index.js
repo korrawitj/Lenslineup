35ef04681d4219a6defa8c526b6dc993c361e895
Gunnpj <gunpengpinij@gmail.com>
2018-09-09 15:41:31 +0700

    sdfsd

--- src/pages/Product/ProductListAdd/ProductListAdd/index.js
+++ src/pages/Product/ProductListAdd/ProductListAdd/index.js
@@ -1,105 +1,105 @@
 import React from 'react'
-import { Input, TreeSelect, Select, Button, Upload, Icon, message, Table, Modal } from 'antd';
-import { connect } from 'react-redux';
-import * as actionCreators from '../../../../store/axios/productcategory';
+import { Input, TreeSelect, Select, Button, Upload, Icon, message, Table, Modal } from 'antd'
+import { connect } from 'react-redux'
+import * as actionCreators from '../../../../store/axios/productcategory'
 const defaultPagination = {
-    pageSizeOptions: ['10', '50', '100', '250'],
-    showSizeChanger: true,
-    current: 1,
-    size: 'small',
-    showTotal: total => `Total ${total} items`,
-    total: 0,
-  }
+  pageSizeOptions: ['10', '50', '100', '250'],
+  showSizeChanger: true,
+  current: 1,
+  size: 'small',
+  showTotal: total => `Total ${total} items`,
+  total: 0,
+}
 const columns = [
-    {
-      title: 'View',
-      key: 'CategoryID',
-      render:  (text, record) => (
-        <span>
+  {
+    title: 'View',
+    key: 'CategoryID',
+    render: (text, record) => (
+      <span>
         <Button icon="cross" size="small" onClick={() => console.log(record)}>
           View
         </Button>
       </span>
-      ),
-    },
-    {
-      title: 'ตัวที่',
-      dataIndex: 'CopyNo',
-      key: 'CopyNo',
-      render: text => (
-        <a className="utils__link--underlined" href="javascript: void(0);">
-          {text}
-        </a>
-      ),
-      sorter: (a, b) => a.CopyNo - b.CopyNo,
-    },
-    {
-      title: 'ชื่อสั้น',
-      dataIndex: 'Serail',
-      key: 'SerialNumber',
-      render: text => (
-        <a className="utils__link--underlined" href="javascript: void(0);">
-          {text}
-        </a>
-      ),
-      sorter: (a, b) => a.SerialNumber - b.SerialNumber,
-    },
-    {
-      title: 'ราคาที่ซื้อ',
-      dataIndex: 'PurchasePrice',
-      key: 'PurchasePrice',
-      render: text => (
-        <a className="utils__link--underlined" href="javascript: void(0);">
-          {'#' + text}
-        </a>
-      ),
-      sorter: (a, b) => a.PurchasePrice - b.PurchasePrice,
-    },
-    {
-        title: 'วันที่ซื้อ',
-        dataIndex: 'PurchaseDate',
-        key: 'PurchaseDate',
-        render: text => (
-          <a className="utils__link--underlined" href="javascript: void(0);">
-            {'#' + text}
-          </a>
-        ),
-        sorter: (a, b) => a.PurchaseDate - b.PurchaseDate,
-      },
-      {
-        title: 'สถานะ',
-        dataIndex: 'Status',
-        key: 'Status',
-        render: text => (
-          <a className="utils__link--underlined" href="javascript: void(0);">
-            {'#' + text}
-          </a>
-        ),
-        sorter: (a, b) => a.Status - b.Status,
-      },
-      {
-        title: 'แก้ไข',
-        key: 'Edit',
-        render: (text, record) => (
-            <span>
-              <Button icon="cross" size="small" onClick={() => console.log(record)}>
-                Edit
-              </Button>
-            </span>
-          ),
-      },
-    {
-      title: 'ลบ',
-      key: 'Action',
-      render: (text, record) => (
-        <span>
-          <Button icon="cross" size="small" onClick={() => console.log(record)}>
-            Remove
-          </Button>
-        </span>
-      ),
-    },
-  ]
+    ),
+  },
+  {
+    title: 'ตัวที่',
+    dataIndex: 'CopyNo',
+    key: 'CopyNo',
+    render: text => (
+      <a className="utils__link--underlined" href="javascript: void(0);">
+        {text}
+      </a>
+    ),
+    sorter: (a, b) => a.CopyNo - b.CopyNo,
+  },
+  {
+    title: 'ชื่อสั้น',
+    dataIndex: 'Serail',
+    key: 'SerialNumber',
+    render: text => (
+      <a className="utils__link--underlined" href="javascript: void(0);">
+        {text}
+      </a>
+    ),
+    sorter: (a, b) => a.SerialNumber - b.SerialNumber,
+  },
+  {
+    title: 'ราคาที่ซื้อ',
+    dataIndex: 'PurchasePrice',
+    key: 'PurchasePrice',
+    render: text => (
+      <a className="utils__link--underlined" href="javascript: void(0);">
+        {'#' + text}
+      </a>
+    ),
+    sorter: (a, b) => a.PurchasePrice - b.PurchasePrice,
+  },
+  {
+    title: 'วันที่ซื้อ',
+    dataIndex: 'PurchaseDate',
+    key: 'PurchaseDate',
+    render: text => (
+      <a className="utils__link--underlined" href="javascript: void(0);">
+        {'#' + text}
+      </a>
+    ),
+    sorter: (a, b) => a.PurchaseDate - b.PurchaseDate,
+  },
+  {
+    title: 'สถานะ',
+    dataIndex: 'Status',
+    key: 'Status',
+    render: text => (
+      <a className="utils__link--underlined" href="javascript: void(0);">
+        {'#' + text}
+      </a>
+    ),
+    sorter: (a, b) => a.Status - b.Status,
+  },
+  {
+    title: 'แก้ไข',
+    key: 'Edit',
+    render: (text, record) => (
+      <span>
+        <Button icon="cross" size="small" onClick={() => console.log(record)}>
+          Edit
+        </Button>
+      </span>
+    ),
+  },
+  {
+    title: 'ลบ',
+    key: 'Action',
+    render: (text, record) => (
+      <span>
+        <Button icon="cross" size="small" onClick={() => console.log(record)}>
+          Remove
+        </Button>
+      </span>
+    ),
+  },
+]
 class ProductListAdd extends React.Component {
   state = {
     title: 'test',
@@ -110,7 +110,6 @@ class ProductListAdd extends React.Component {
   render() {
     let { pager } = this.state
     return (
-        
       <div className="card">
         <div className="card-header">
           <div className="utils__title">
@@ -180,34 +179,33 @@ class ProductListAdd extends React.Component {
               </div>
             </div>
             <div className="col-lg-4">
-            <div className="row">
-            <div className="col-lg-12">
-            <div className="form-group">
+              <div className="row">
+                <div className="col-lg-12">
+                  <div className="form-group">
                     <label htmlFor="product-edit-title">รูปภาพ</label>
-                   
                   </div>
-                  </div>
-            </div>
+                </div>
+              </div>
             </div>
             <div className="col-lg-8">
-            <div className="utils__title">
-            <strong>Product Copy</strong>
-          </div>
-            <Table
-              columns={columns}
-              dataSource=''
-              pagination={pager}
-              onChange={this.handleTableChange}
-            />
-          </div>
+              <div className="utils__title">
+                <strong>Product Copy</strong>
+              </div>
+              <Table
+                columns={columns}
+                dataSource=""
+                pagination={pager}
+                onChange={this.handleTableChange}
+              />
+            </div>
           </div>
           <div className="col-lg-12">
-                  <div className="form-actions">
-                 <Button type="primary" className="mr-2">
-                 Save Product
-               </Button>
-               <Button type="default">Cancel</Button>
-               </div>            
+            <div className="form-actions">
+              <Button type="primary" className="mr-2">
+                Save Product
+              </Button>
+              <Button type="default">Cancel</Button>
+            </div>
           </div>
         </div>
       </div>
