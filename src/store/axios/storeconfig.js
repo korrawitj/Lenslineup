fb8e60916f8619d2d9a09a8466dc241ebd3f1374
Korrawit Jaijit <korrawit.jaijit@gmail.com>
2018-09-09 21:46:13 +0700

    add axios

--- src/store/axios/storeconfig.js
+++ src/store/axios/storeconfig.js
@@ -2,27 +2,27 @@ import axios from 'axios'
 import * as actionCreators from '../actions/index'
 
 export function getAllDataHoliday() {
-    return dispatch => {
-        return axios
-            .get('/api/masterHoliday/getAllHoliday')
-            .then(response => {
-                dispatch(actionCreators.getdata(response.data.holidayData))
-            })
-            .catch(error => {
-                console.log('Error axios ' + error)
-            })
-    }
+  return dispatch => {
+    return axios
+      .get('/api/masterHoliday/getAllHoliday')
+      .then(response => {
+        dispatch(actionCreators.getdata(response.data.holidayData))
+      })
+      .catch(error => {
+        console.log('Error axios ' + error)
+      })
+  }
 }
 
 export function getAllDataHolidayShop() {
-    return dispatch => {
-        return axios
-            .get('/api/masterHoliday/getAllHolidayShop')
-            .then(response => {
-                dispatch(actionCreators.getdata(response.data.holidayShopData))
-            })
-            .catch(error => {
-                console.log('Error axios ' + error)
-            })
-    }
+  return dispatch => {
+    return axios
+      .get('/api/masterHoliday/getAllHolidayShop')
+      .then(response => {
+        dispatch(actionCreators.getdata(response.data.holidayShopData))
+      })
+      .catch(error => {
+        console.log('Error axios ' + error)
+      })
+  }
 }
