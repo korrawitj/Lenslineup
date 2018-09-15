99485fd2fd143f075e21e2e3ba7ed3443d97ea5a
Gunnpj <gunpengpinij@gmail.com>
2018-09-15 20:35:40 +0700

    asdasd

--- src/pages/Config/ConfigParameter/HolidayShop/index.js
+++ src/pages/Config/ConfigParameter/HolidayShop/index.js
@@ -136,29 +136,32 @@ class HolidayShop extends React.Component {
         title: 'การรับ',
         dataIndex: 'receive',
         key: 'receive',
-        render: text => <span>
-              <Icon
+        render: text => (
+          <span>
+            <Icon
               style={{ fontSize: '25px' }}
               theme="twoTone"
               twoToneColor={text ? '#52c41a' : '#eb2f96'}
               type={text ? 'check-circle' : 'close-circle'}
             />
-
-        </span>,
+          </span>
+        ),
         sorter: (a, b) => a.receive - b.receive,
       },
       {
         title: 'การคืน',
         dataIndex: 'recurring',
         key: 'recurring',
-        render: text => <span>
-               <Icon
+        render: text => (
+          <span>
+            <Icon
               style={{ fontSize: '25px' }}
               theme="twoTone"
               twoToneColor={text ? '#52c41a' : '#eb2f96'}
               type={text ? 'check-circle' : 'close-circle'}
             />
-        </span>,
+          </span>
+        ),
         sorter: (a, b) => a.receive - b.receive,
       },
       {
