499db073053eaa91842e37752a6f0d9887fd492b
Gunnpj <gunpengpinij@gmail.com>
2018-09-22 12:39:15 +0700

    push

---
+++ src/pages/Product/ProductDetail/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Page from 'components/LayoutComponents/Page'
+import Helmet from 'react-helmet'
+import ProductDetail from './ProductDetail'
+
+class ProductPage extends React.Component {
+  static defaultProps = {
+    pathName: 'อุปกรณ์',
+    roles: ['agent', 'administrator'],
+  }
+
+  render() {
+    const props = this.props
+    return (
+      <Page {...props}>
+        <Helmet title="อุปกรณ์" />
+        <ProductDetail />
+      </Page>
+    )
+  }
+}
+
+export default ProductPage
