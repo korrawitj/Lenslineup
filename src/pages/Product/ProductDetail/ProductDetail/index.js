9eba3049414dde5d0d1c1f90494188ce2d9e205d
Korrawit Jaijit <korrawit.jaijit@gmail.com>
2018-09-23 00:21:48 +0700

    edit grid

--- src/pages/Product/ProductDetail/ProductDetail/index.js
+++ src/pages/Product/ProductDetail/ProductDetail/index.js
@@ -154,8 +154,8 @@ const CollectionCreateForm = Form.create()(
                   })(<Input />)}
                 </FormItem>
               ) : (
-                  ''
-                )}
+                ''
+              )}
               <FormItem {...formItemLayout} label="ราคาเช่าครึ่งวัน">
                 {getFieldDecorator('productItemData.IsHaftDay')(<Checkbox />)}
               </FormItem>
@@ -164,8 +164,8 @@ const CollectionCreateForm = Form.create()(
                   {getFieldDecorator('productItemData.RentHalfDay_Fee')(<Input />)}
                 </FormItem>
               ) : (
-                  ''
-                )}
+                ''
+              )}
               <FormItem {...formItemLayout} label="ราคาเช่าหนึ่งชั่วโมง">
                 {getFieldDecorator('productItemData.IsHour')(<Checkbox />)}
               </FormItem>
@@ -174,8 +174,8 @@ const CollectionCreateForm = Form.create()(
                   {getFieldDecorator('productItemData.RentHour_Fee')(<Input />)}
                 </FormItem>
               ) : (
-                  ''
-                )}
+                ''
+              )}
               <FormItem {...formItemLayout} label="จำนวนวันขั้นต่ำที่ให้เช่า">
                 {getFieldDecorator('productItemData.RentDay')(<Input />)}
               </FormItem>
