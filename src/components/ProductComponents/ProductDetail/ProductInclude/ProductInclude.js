7041b3ae966bc9eda29d313727650badaab85c5d
Gunnpj <gunpengpinij@gmail.com>
2018-09-24 12:49:54 +0700

    123123

--- src/components/ProductComponents/ProductDetail/ProductInclude/ProductInclude.js
+++ src/components/ProductComponents/ProductDetail/ProductInclude/ProductInclude.js
@@ -1,7 +1,207 @@
 import React from 'react'
+import {Icon, Input, Button, Modal ,Table} from 'antd'
 
+const defaultPagination = {
+  pageSizeOptions: ['10', '50', '100', '250'],
+  showSizeChanger: true,
+  current: 1,
+  size: 'small',
+  showTotal: total => `Total ${total} items`,
+  total: 0,
+}
 class ProductInclude extends React.Component {
-  render() {
+  state = {
+    pager: { ...defaultPagination },
+    filterDropdownVisible: false,
+    searchText: '',
+    filtered: false,
+    previewVisible: false,
+  }
+  onSearch = () => {
+    const { searchText, tableData } = this.state
+    let reg = new RegExp(searchText, 'gi')
+    this.setState({
+      filterDropdownVisible: false,
+      filtered: !!searchText,
+      data: tableData
+        .map(record => {
+          let match = record.ProductName.match(reg)
+          if (!match) {
+            return null
+          }
+          return {
+            ...record,
+            name: (
+              <span>
+                {record.ProductName.split(reg).map(
+                  (text, i) =>
+                    i > 0 ? [<span className="highlight">{match[0]}</span>, text] : text,
+                )}
+              </span>
+            ),
+          }
+        })
+        .filter(record => !!record),
+    })
+  }
+  showDeleteConfirm(record, props) {
+    let T = record
+    Modal.confirm({
+      title: 'Are you sure delete this row?',
+      content: <div>Delelte Product Item = {record.ItemID}</div>,
+      okText: 'Yes',
+      okType: 'danger',
+      cancelText: 'No',
+      onOk() {
+        props.deleteProductItem(record.ItemID)
+      },
+      onCancel() {
+        console.log('Cancel')
+      },
+    })
+  }
+
+  showData(record) {
+    let T = record
+    Modal.info({
+      title: <div>อุปกรณ์จัดชุด {record.ItemID}</div>,
+      width: 1000,
+
+      content: (
+        <div className="row">
+          <div className="col-md-4">
+            <label>อุปกรณ์</label>
+          </div>
+          <div className="col-md-6">{record.Name}</div>
+          <div className="col-md-4">
+            <label>ราคาในสัญญา</label>
+          </div>
+          <div className="col-md-6">{record.ContractPrice}</div>
+          <div className="col-md-4">
+            <label>จำนวน</label>
+          </div>
+          <div className="col-md-6">{record.Quantity}</div>
+        </div>
+      ),
+      onOk() {},
+    })
+  }
+  handleTableChange = (pagination, filters, sorter) => {
+    if (this.state.pager) {
+      const pager = { ...this.state.pager }
+      if (pager.pageSize !== pagination.pageSize) {
+        this.pageSize = pagination.pageSize
+        pager.pageSize = pagination.pageSize
+        pager.current = 1
+      } else {
+        pager.current = pagination.current
+      }
+      this.setState({
+        pager: pager,
+      })
+    }
+  }
+  
+  render() 
+  {
+    let { pager, data } = this.state
+    const columns = [
+      {
+        title: 'ItemID',
+        dataIndex: 'ItemID',
+        key: 'ItemID',
+        render: text => (
+          <a className="utils__link--underlined" href="javascript: void(0);">
+            {'#' + text}
+          </a>
+        ),
+        sorter: (a, b) => a.ProductID - b.ProductID,
+      },
+      {
+        title: 'อุปกรณ์',
+        dataIndex: 'Name',
+        key: 'Name',
+        sorter: (a, b) => a.Copy.length - b.Copy.length,
+        render: text => (
+          <a className="utils__link--underlined" href="javascript: void(0);">
+            {text}
+          </a>
+        ),
+        filterDropdown: (
+          <div className="custom-filter-dropdown">
+            <Input
+              ref={ele => (this.searchInput = ele)}
+              placeholder="Search name"
+              value={this.state.searchText}
+              onChange={this.onInputChange}
+              onPressEnter={this.onSearch}
+            />
+            <Button type="primary" onClick={this.onSearch}>
+              Search
+            </Button>
+          </div>
+        ),
+        filterIcon: (
+          <Icon type="search" style={{ color: this.state.filtered ? '#108ee9' : '#aaa' }} />
+        ),
+        filterDropdownVisible: this.state.filterDropdownVisible,
+        onFilterDropdownVisibleChange: visible => {
+          this.setState(
+            {
+              filterDropdownVisible: visible,
+            },
+            () => this.searchInput && this.searchInput.focus(),
+          )
+        },
+      },
+      {
+        title: 'ราคาในสัญญา',
+        dataIndex: 'ContractPrice',
+        key: 'ContractPrice',
+        render: text => <span>{text}</span>,
+        sorter: (a, b) => a.PromisePrice - b.PromisePrice,
+      },
+      {
+        title: 'จำนวน',
+        dataIndex: 'Quantity',
+        key: 'Quantity',
+        render: text => <span>{text}</span>,
+        sorter: (a, b) => a.Quantity - b.Quantity,
+      },
+      {
+        title: 'Note',
+        dataIndex: 'Note',
+        key: 'Note',
+        render: text => <span>{text}</span>,
+        sorter: (a, b) => a.Note - b.Note,
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        render: (text, record) => (
+          <span>
+            <Button
+              shape="circle"
+              icon="search"
+              onClick={() => this.showData(record)}
+              className="palm-btn-primary"
+            />
+            <Button
+              shape="circle"
+              icon="edit"
+              onClick={() => this.onEditProductItem(record, this.props)}
+              className="palm-btn-warning"
+            />
+            <Button
+              type="danger"
+              shape="circle"
+              icon="delete"
+              onClick={() => this.showDeleteConfirm(record, this.props)}
+            />
+          </span>
+        ),
+      },
+    ]
     return (
       <div className="card">
         <div className="card-header">
@@ -11,6 +211,12 @@ class ProductInclude extends React.Component {
         </div>
         <hr />
         <div className="card-body" />
+         <Table
+            columns={columns}
+            // dataSource={this.props.productItemData.productItemData}
+            pagination={pager}
+            onChange={this.handleTableChange}
+          /> 
       </div>
     )
   }
