91b8d6703c276ffe117fe1f79471bd0f97ccf3cd
korrawitj <korrawit.jaijit@gmail.com>
2018-09-30 19:55:46 +0700

    add order detail

--- src/pages/Order/OrderDetail/OrderDetail/index.js
+++ src/pages/Order/OrderDetail/OrderDetail/index.js
@@ -1,13 +1,272 @@
 import React from 'react'
+import {
+  Table,
+  Icon,
+  Input,
+  InputNumber,
+  Button,
+  Modal,
+  Upload,
+  DatePicker,
+  Radio,
+  Form,
+  Checkbox,
+  Select,
+  TreeSelect,
+  message,
+} from 'antd'
+import { connect } from 'react-redux'
+import moment from 'moment'
+import { relative } from 'path'
+import './detail.css'
 
-class OrderList extends React.Component {
+const FormItem = Form.Item
+const RadioGroup = Radio.Group
+const RadioButton = Radio.Button
+const confirm = Modal.confirm
+const { TextArea } = Input
+
+const CollectionCreateForm = Form.create()(
+  class extends React.Component {
+    render() {
+      const {
+        visible,
+        onCancel,
+        onCreate,
+        form,
+        orderData,
+      } = this.props
+      const { getFieldDecorator } = form
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-7">
+              <div className="card">
+                <div className="card-header">
+                  <div className="utils__title">
+                    <strong>เพิ่มรายการจอง</strong>
+                  </div>
+                </div>
+                <hr />
+                <div className="card-body">
+                  <Form>
+                    <div className="row">
+                      <div className="col-md-3 labelcenter">
+                        <label>อุปกรณ์ : </label>
+                      </div>
+                      <div className="col-md-9 inputcenter">
+                        <FormItem className="inputcenter">
+                          {getFieldDecorator('orderData.Name', {
+                            initialValue: orderData.Name,
+                          })(<Input />)}
+                        </FormItem>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-3 labelcenter">
+                        <label>วันที่รับ : </label>
+                      </div>
+                      <div className="col-md-9 inputcenter">
+                        <FormItem className="inputcenter">
+                          {getFieldDecorator('orderData.ReceiveDate')(<DatePicker />)}
+                        </FormItem>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-3 labelcenter">
+                        <label>วันที่คืน : </label>
+                      </div>
+                      <div className="col-md-9 inputcenter">
+                        <FormItem className="inputcenter">
+                          {getFieldDecorator('orderData.RestoreDate')(<DatePicker />)}
+                        </FormItem>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-12 labelcenter">
+                        <label>วันที่คืน : </label>
+                      </div>
+                    </div>
+                  </Form>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-5">
+              <div className="row">
+                <div className="col-md-12">
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-12">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="card">
+                <div className="card-header">
+                  <div className="utils__title">
+                    <strong></strong>
+                  </div>
+                </div>
+                <hr />
+                <div className="card-body" />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="card">
+                <div className="card-body" />
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }
+  },
+)
+
+const defaultPagination = {
+  pageSizeOptions: ['10', '50', '100', '250'],
+  showSizeChanger: true,
+  current: 1,
+  size: 'small',
+  showTotal: total => `Total ${total} items`,
+  total: 0,
+}
+
+class OrderDetail extends React.Component {
+  state = {
+    pager: { ...defaultPagination },
+    orderData: {},
+    filterDropdownVisible: false,
+    searchText: '',
+    filtered: false,
+    previewVisible: false,
+    previewImage: '',
+    visible: false,
+    fileList: [
+      {
+        uid: -1,
+        name: 'xxx.png',
+        status: 'done',
+        url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+      },
+    ],
+  }
+
+  handleCancel = () => this.setState({ previewVisible: false, visible: false })
+
+  showModal = () => {
+    this.setState({ visible: true })
+  }
+
+  handleCreate = () => {
+    const form = this.formRef.props.form
+    form.validateFields((err, values) => {
+      if (err) {
+        return
+      }
+      form.resetFields()
+      this.setState({ visible: false })
+    })
+  }
+
+  saveFormRef = formRef => {
+    this.formRef = formRef
+  }
+
+  handlePreview = file => {
+    this.setState({
+      previewImage: file.url || file.thumbUrl,
+      previewVisible: true,
+    })
+  }
+
+  handleChange = ({ fileList }) => this.setState({ fileList })
+
+  showDeleteConfirm(record) {
+    let T = record
+    confirm({
+      title: 'Are you sure delete this row?',
+      content: <div>Delelte ProductID = {record.id}</div>,
+      okText: 'Yes',
+      okType: 'danger',
+      cancelText: 'No',
+      onOk() {
+        console.log('OK')
+      },
+      onCancel() {
+        console.log('Cancel')
+      },
+    })
+  }
+
+  onInputChange = e => {
+    this.setState({ searchText: e.target.value })
+  }
+
+  onSearch = () => {
+    const { searchText, tableData } = this.state
+    let reg = new RegExp(searchText, 'gi')
+    this.setState({
+      filterDropdownVisible: false,
+      filtered: !!searchText,
+      data: tableData
+        .map(record => {
+          let match = record.ProductName.match(reg)
+          if (!match) {
+            return null
+          }
+          return {
+            ...record,
+            name: (
+              <span>
+                {record.ProductName.split(reg).map(
+                  (text, i) =>
+                    i > 0 ? [<span className="highlight">{match[0]}</span>, text] : text,
+                )}
+              </span>
+            ),
+          }
+        })
+        .filter(record => !!record),
+    })
+  }
+
+  handleTableChange = (pagination, filters, sorter) => {
+    if (this.state.pager) {
+      const pager = { ...this.state.pager }
+      if (pager.pageSize !== pagination.pageSize) {
+        this.pageSize = pagination.pageSize
+        pager.pageSize = pagination.pageSize
+        pager.current = 1
+      } else {
+        pager.current = pagination.current
+      }
+      this.setState({
+        pager: pager,
+      })
+    }
+  }
+
   render() {
     return (
-      <div className="card">
-        <p>Hello</p>
+      <div>
+        <CollectionCreateForm
+          wrappedComponentRef={this.saveFormRef}
+          orderData={this.state.orderData}
+          // productCate={this.props.product.productCate}
+          // productItem={this.props.product.productItemData}
+          visible={this.state.visible}
+          onCancel={this.handleCancel}
+          onCreate={this.handleCreate}
+        />
       </div>
     )
   }
 }
 
-export default OrderList
+export default OrderDetail
