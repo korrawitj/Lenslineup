cd6cf10dd71df56c3bc3c7ee5220ebaf7172a308
korrawitj <korrawit.jaijit@gmail.com>
2018-10-03 23:08:09 +0700

    edit reducer

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -2,7 +2,7 @@ import * as actionTypes from '../actions/actionType'
 import { updateObject } from '../utility'
 
 const initialState = {
-  orderDetailData: []
+  orderDetailData: [],
 }
 
 const reducer = (state = initialState, action) => {
